b1cd61b9208ccf7afab7808e163a62c7215f80fa
younghoshin <sim6671@naver.com>
2023-06-30 11:01:22 +0900

    init: 초기 생성

---
+++ server/src/main/java/com/seb44main011/petplaylist/PetplaylistApplication.java
@@ -0,0 +1,15 @@
+package com.seb44main011.petplaylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class PetplaylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetplaylistApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/seb44main011/petplaylist/PetplaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seb44main011.petplaylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetplaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
