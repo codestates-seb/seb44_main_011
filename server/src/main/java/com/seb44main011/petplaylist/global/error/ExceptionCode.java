ccd1f2a1a4d257bb3dc9bf214c2a1a8333942c54
KYUNGMINMON <rudals7019@gmail.com>
2023-06-30 17:27:27 +0900

    :fire: ExceptionCode 수정

--- server/src/main/java/com/seb44main011/petplaylist/global/error/ExceptionCode.java
+++ server/src/main/java/com/seb44main011/petplaylist/global/error/ExceptionCode.java
@@ -10,16 +10,8 @@ public enum ExceptionCode {
     EXPIRED_TOKEN(401,"Token has expired"),
     ACCOUNT_RESTRICTED(403,"oauth account user"),
     USER_NOT_FOUND(404, "Member not found"),
-    QUESTION_NOT_FOUND(404, "Question not found"),
-    CANNOT_CHANGE_QUESTION(403,"Question can not change"),
-    CANNOT_DELETE_QUESTION(403,"Question can not delete"),
-    ANSWER_NOT_FOUND(404, "Answer not found"),
     USER_EXISTS(409,"USER EXISTS"),
-    USER_MISMATCH(403,"The login user and the author are different"),
-    QUESTION_VOTE_ALREADY(403, "Already question voted"),
-    QUESTION_VOTE_NOT_FOUND(404, "Question vote not found"),
-    ANSWER_VOTE_ALREADY(403, "Already answer voted"),
-    ANSWER_VOTE_NOT_FOUND(404, "Answer vote not found");
+    USER_MISMATCH(403,"The login user and the author are different");
 
     @Getter
     private int status;
