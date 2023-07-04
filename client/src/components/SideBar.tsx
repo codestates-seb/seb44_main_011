c32a42b045c763a3d10f1677798c95e5dd981b4f
kimdongju666 <kimdongju666@naevr.com>
2023-07-04 20:55:45 +0900

    ✅ feat : 라우팅 틀 작업

---
+++ client/src/components/SideBar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function SideBar() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">Tags</a>
+        </li>
+        <li>
+          <a href="/about">MyList</a>
+        </li>
+        <li>
+          <a href="/about">MyPage</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default SideBar;
---
+++ client/src/pages/Mypage.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import SideBar from '../components/SideBar'
+
+function Mypage() {
+  return (
+    <>
+    <div>Mypage</div>
+    <SideBar />
+    </>
+    
+  )
+}
+
+export default Mypage
