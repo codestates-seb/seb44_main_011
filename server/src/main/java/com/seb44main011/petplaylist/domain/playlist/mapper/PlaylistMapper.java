53e345b08e2b3683f3eab42538935c0c777d2dda
younghoshin <sim6671@naver.com>
2023-07-05 16:59:45 +0900

    :sparkles: 개인 플레이 리스트 추가하기 기능 구현

--- server/src/main/java/com/seb44main011/petplaylist/domain/playlist/mapper/PlaylistMapper.java
+++ server/src/main/java/com/seb44main011/petplaylist/domain/playlist/mapper/PlaylistMapper.java
@@ -1,4 +1,19 @@
 package com.seb44main011.petplaylist.domain.playlist.mapper;
 
-public class PlaylistMapper {
+import com.seb44main011.petplaylist.domain.member.entity.Member;
+import com.seb44main011.petplaylist.domain.music.entity.Music;
+import com.seb44main011.petplaylist.domain.playlist.entity.MusicList;
+import com.seb44main011.petplaylist.domain.playlist.entity.PersonalPlayList;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING,unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface PlaylistMapper {
+   default MusicList MemberAndMusicToMusicList(PersonalPlayList playList, Music music){
+       return MusicList.builder()
+               .personalPlayList(playList)
+               .music(music)
+               .build();
+   }
 }
