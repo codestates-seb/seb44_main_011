5a4fb1f420ebd88ebf16753fb5c98f92367a21ee
younghoshin <sim6671@naver.com>
2023-07-05 09:14:51 +0900

    :sparkles: Music Playlist 디렉토리 구조 추가 및 Music 컨트롤러 추가

---
+++ server/src/main/java/com/seb44main011/petplaylist/domain/music/mapper/MusicMapper.java
@@ -0,0 +1,13 @@
+package com.seb44main011.petplaylist.domain.music.mapper;
+
+import com.seb44main011.petplaylist.domain.music.dto.MusicDto;
+import com.seb44main011.petplaylist.domain.music.entity.Music;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING,unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface MusicMapper {
+    MusicDto.PublicResponse publicResponseToMusic(Music music);
+    MusicDto.ApiResponse apiResponseToMusic(Music music);
+}
---
+++ server/src/main/java/com/seb44main011/petplaylist/domain/playlist/service/PlaylistService.java
@@ -0,0 +1,9 @@
+package com.seb44main011.petplaylist.domain.playlist.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PlaylistService {
+}
