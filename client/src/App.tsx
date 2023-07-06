e4df0ce6ba1a72eebeffac9dccf37fe94959712b
seobi-97 <71924843+seobi-97@users.noreply.github.com>
2023-07-06 17:05:48 +0900

    🎨 Component 세분화 작업 및 디자인 작업

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,13 @@
 import { ReactComponent as Dog } from "./assets/icons/dog.svg";
-
+import Login from "./pages/Login/Login";
+import SignUp from "./pages/SignUp/SignUp";
+import Delete from "./pages/Delete/Delete";
 function App() {
   return (
     <>
+      {/* <Login />
+      <SignUp />
+      <Delete /> */}
       <div>
         <Dog fill="red" />
       </div>
