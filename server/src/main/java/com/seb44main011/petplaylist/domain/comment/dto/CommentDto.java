8593cb1b21e8950fd926f172f7cbb0883f5f0153
RedJunKi <bing8821@naver.com>
2023-07-06 17:00:27 +0900

    :necktie: 댓글 수정 테스트 완료

--- server/src/main/java/com/seb44main011/petplaylist/domain/comment/dto/CommentDto.java
+++ server/src/main/java/com/seb44main011/petplaylist/domain/comment/dto/CommentDto.java
@@ -2,10 +2,7 @@ package com.seb44main011.petplaylist.domain.comment.dto;
 
 import com.seb44main011.petplaylist.domain.member.entity.Member;
 import com.seb44main011.petplaylist.domain.music.entity.Music;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -24,6 +21,8 @@ public class CommentDto {
     }
 
     @Getter @Setter
+    @AllArgsConstructor
+    @Builder
     public static class Patch {
         private Long commentId;
         @Size(min = 2 , max = 30)
--- server/src/main/java/com/seb44main011/petplaylist/global/error/ExceptionCode.java
+++ server/src/main/java/com/seb44main011/petplaylist/global/error/ExceptionCode.java
@@ -13,7 +13,8 @@ public enum ExceptionCode {
     MEMBER_EXISTS(409,"Member EXISTS"),
     MEMBER_MISMATCH(403,"The login Member and the author are different"),
     MEMBER_WITHDRAWN(409, "Already with drawn Member"),
-    MUSIC_NOT_FOUND(404,"Music not found");
+    MUSIC_NOT_FOUND(404,"Music not found"),
+    COMMENT_NOT_FOUND(404, "Comment not found");
 
     @Getter
     private int status;
