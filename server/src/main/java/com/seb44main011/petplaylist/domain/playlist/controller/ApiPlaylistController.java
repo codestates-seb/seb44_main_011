90b4080796d43dad9c747ec1c42fd4bfcfd149ab
younghoshin <sim6671@naver.com>
2023-07-08 23:47:03 +0900

    :recycle: 깃 플로우 변경

--- server/src/main/java/com/seb44main011/petplaylist/domain/playlist/controller/ApiPlaylistController.java
+++ server/src/main/java/com/seb44main011/petplaylist/domain/playlist/controller/ApiPlaylistController.java
@@ -12,7 +12,7 @@ import com.seb44main011.petplaylist.domain.playlist.mapper.PlaylistMapper;
 import com.seb44main011.petplaylist.domain.playlist.service.MusicListService;
 import com.seb44main011.petplaylist.domain.playlist.service.PlaylistService;
 import com.seb44main011.petplaylist.global.common.MultiResponseDto;
-import com.seb44main011.petplaylist.global.stubData.StubData;
+//import com.seb44main011.petplaylist.global.stubData.StubData;
 import com.seb44main011.petplaylist.global.utils.UriCreator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -38,10 +38,10 @@ public class ApiPlaylistController {
     private final MusicListService musicListService;
     private final MusicService musicService;
     private final StubData stubData;
-    @PostMapping("/test")
-    public void postTest(){
-        stubData.insertData();
-    }
+//    @PostMapping("/test")
+//    public void postTest(){
+//        stubData.insertData();
+//    }
 
     @PostMapping(value = "/{member-id}", name = "music_name")
     public ResponseEntity<?> postPersonalPlayList(@PathVariable("member-id")@Positive long id,
