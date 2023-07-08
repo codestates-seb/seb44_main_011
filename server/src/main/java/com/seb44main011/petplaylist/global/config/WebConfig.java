db8c6fa538107fac22ddcb5bdcccb8cf85a9f765
younghoshin <sim6671@naver.com>
2023-07-08 22:54:30 +0900

    :recycle: gitignore 변경

--- server/src/main/java/com/seb44main011/petplaylist/global/config/WebConfig.java
+++ server/src/main/java/com/seb44main011/petplaylist/global/config/WebConfig.java
@@ -4,6 +4,7 @@ import com.seb44main011.petplaylist.global.handler.AuthenticationArgumentResolve
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -17,5 +18,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers){
         resolvers.add(authenticationArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("*")
+                .maxAge(3000);
     }
 }
