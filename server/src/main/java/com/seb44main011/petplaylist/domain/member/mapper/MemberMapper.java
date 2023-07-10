557b973f21e86877cda7eda18c2c1549b6bc3731
KYUNGMINMON <rudals7019@gmail.com>
2023-07-10 13:49:29 +0900

    :necktie: 회원정보 조회 기능 추가

--- server/src/main/java/com/seb44main011/petplaylist/domain/member/mapper/MemberMapper.java
+++ server/src/main/java/com/seb44main011/petplaylist/domain/member/mapper/MemberMapper.java
@@ -2,8 +2,15 @@ package com.seb44main011.petplaylist.domain.member.mapper;
 
 import com.seb44main011.petplaylist.domain.member.dto.MemberDto;
 import com.seb44main011.petplaylist.domain.member.entity.Member;
+import com.seb44main011.petplaylist.domain.music.entity.Music;
+import com.seb44main011.petplaylist.domain.playlist.dto.PlaylistDto;
+import com.seb44main011.petplaylist.domain.playlist.entity.entityTable.MusicList;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface MemberMapper {
@@ -11,11 +18,26 @@ public interface MemberMapper {
 
     Member memberDtoLogInPostToMember(MemberDto.LogInPost memberDtoLogInPost);
 
-    Member memberDtoPatchToMember(MemberDto.Patch memberDtoPatch);
 
     MemberDto.SignUpResponse memberToMemberDtoSignUpResponse(Member member);
 
     MemberDto.LogInResponse memberToMemberDtoLogInResponse(Member member);
 
     MemberDto.PatchResponse memberToMemberDtoPatchResponse(Member member);
+
+    default MemberDto.MyPageResponse memberToMyPageResponse(Member member) {
+        List<Music> musicList = new ArrayList<>();
+        for (int i = 0; i < 6 ; i++) {
+            if (member.getPersonalPlayList().getMusicLists().get(i).getMusic() == null) {
+                break;
+            }
+            else musicList.add(member.getPersonalPlayList().getMusicLists().get(i).getMusic());
+        }
+
+        return MemberDto.MyPageResponse.builder()
+                .email(member.getEmail())
+                .name(member.getName())
+                .musicLists(musicList)
+                .build();
+    }
 }
