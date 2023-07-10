0b8cc1f5e52cc4e67cb0ad4b7cbd67ab2d676d0d
심지훈 <40737888+wlgns2223@users.noreply.github.com>
2023-07-10 21:11:22 +0900

    chore: 테스트입니다

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import Mypage from "./pages/Mypage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Tags from "./pages/Tags";
 
+console.log("ff")
+
 function App() {
   return (
     <BrowserRouter>
