2c8ae8e4bf596fd0e91f2fa25522ca12b3e21bd4
RedJunKi <bing8821@naver.com>
2023-07-13 17:43:37 +0900

    :necktie: 댓글 리팩토링 완료

--- server/src/main/java/com/seb44main011/petplaylist/PetplaylistApplication.java
+++ server/src/main/java/com/seb44main011/petplaylist/PetplaylistApplication.java
@@ -3,6 +3,7 @@ package com.seb44main011.petplaylist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
