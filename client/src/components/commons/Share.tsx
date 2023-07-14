d14b73e5e1ba22cc727bdcb1e7781bfa3f9ced57
seobi-97 <71924843+seobi-97@users.noreply.github.com>
2023-07-14 11:36:59 +0900

    🎨 useGoogleLogin

--- client/src/components/commons/Share.tsx
+++ client/src/components/commons/Share.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import Google from "../../assets/imgs/google.svg";
 import Naver from "../../assets/imgs/naver.svg";
 import Kakao from "../../assets/imgs/kakao.svg";
+import { useGoogleLogin } from "@react-oauth/google";
 
 const ShareContainer = styled.div`
   width: 200px;
@@ -13,9 +14,16 @@ const ShareContainer = styled.div`
   }
 `;
 function Share() {
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => console.log(codeResponse),
+    onError: (errorResponse) => {
+      console.error(errorResponse);
+    },
+    flow: "auth-code",
+  });
   return (
     <ShareContainer>
-      <img src={Google} alt="Google" />
+      <img onClick={() => login()} src={Google} alt="Google" />
       <img src={Naver} alt="Naver" />
       <img src={Kakao} alt="Kakao" />
     </ShareContainer>
