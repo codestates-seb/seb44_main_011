f5bf1f46f44524462af5c53f968e11b539b50299
seobi-97 <71924843+seobi-97@users.noreply.github.com>
2023-07-25 23:59:20 +0900

    🎨 비밀번호 정규식 수정

--- client/src/utils/Check.ts
+++ client/src/utils/Check.ts
@@ -3,4 +3,6 @@ export const EmailRegEx =
   /^[A-Za-z0-9]([-_.]?[A-Za-z0-9])*@[A-Za-z0-9]([-_.]?[A-Za-z0-9])*\.[A-Za-z]{2,3}$/i;
 export const PasswordMin = 8;
 export const PasswordMax = 16;
-export const PasswordRegEx = /^(?=.*[A-Za-z])(?=.*[0-9]).{8,16}$/;
+export const PasswordRegEx = /^[A-Za-z0-9]+$/;
+//export const PasswordRegEx = /^(?=.*[A-Za-z])(?=.*[0-9]).{8,16}$/;
+//export const reg = /[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/gi;
